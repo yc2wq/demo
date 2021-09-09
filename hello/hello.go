1f986c3c4f98c770d99bd0bb9d8c7fe5dd0dcd51
tomxu <tomxu@yunify.com>
2021-09-09 18:06:26 +0800

    Add code

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,14 @@
 package hello
 
+func Add(a int, b int) int {
+    return a + b
+}
+
+func Mul(a int, b int) int {
+    return a * b
+}
+
 // Greet ... Greet GitHub Actions
 func Greet() string {
-  return "Hello GitHub Actions. tomxu.top is awesome"
+	return "Hello GitHub Actions. tomxu.top is awesome"
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,6 +2,16 @@ package hello
 
 import "testing"
 
+func TestAdd(t *testing.T) {
+	if ans := Add(1, 2); ans != 3 {
+		t.Errorf("1 + 2 expected be 3, but %d got", ans)
+	}
+
+	if ans := Add(-10, -20); ans != -30 {
+		t.Errorf("-10 + -20 expected be -30, but %d got", ans)
+	}
+}
+
 func TestGreet(t *testing.T) {
 	result := Greet()
 	if result != "Hello GitHub Actions. tomxu.top is awesome" {
