874abcef30cfb4434d0ec5d96234d95a5119990f
tomxu <tomxu@yunify.com>
2021-09-10 10:31:59 +0800

    Add comment

--- hello/hello.go
+++ hello/hello.go
@@ -1,17 +1,22 @@
 package hello
 
+// Add func addition
 func Add(a int, b int) int {
     return a + b
 }
 
+
+// Sub func subtraction
 func Sub(a int, b int) int {
     return a - b
 }
 
+// Mul func multiplication
 func Mul(a int, b int) int {
     return a * b
 }
 
+// Division func
 func Division(a int, b int) int {
     return a / b
 }
