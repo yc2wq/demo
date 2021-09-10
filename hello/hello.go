d8f5547043a2a8e9e06b2a6dcb821e60b45a9b87
tomxu <tomxu@yunify.com>
2021-09-10 10:17:58 +0800

    Add code

--- hello/hello.go
+++ hello/hello.go
@@ -12,6 +12,10 @@ func Mul(a int, b int) int {
     return a * b
 }
 
+func Division(a int, b int) int {
+    return a / b
+}
+
 // Greet ... Greet GitHub Actions
 func Greet() string {
 	return "Hello GitHub Actions. tomxu.top is awesome"
